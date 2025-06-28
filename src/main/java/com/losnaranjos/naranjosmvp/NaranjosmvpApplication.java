fcce105bc71691a364ec36d45c3fc470ab01f0bf
Alorca <aa.lorca@duocuc.cl>
2025-06-28 17:44:54 -0400

    Ver 1.0 Inicialiazación Documentacion

---
+++ src/main/java/com/losnaranjos/naranjosmvp/NaranjosmvpApplication.java
@@ -0,0 +1,13 @@
+package com.losnaranjos.naranjosmvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaranjosmvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaranjosmvpApplication.class, args);
+	}
+
+}
